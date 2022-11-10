ad0be37023b84c8f9f55d4c03f8bac852616ca4a
JoelMendozaP <joelmendozapaucara@gmail.com>
2022-11-10 00:51:31 -0400

    Aprendiendo eventos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [color, setcolor] = useState('red')
+
+  function onSubmit() {
+    const simbolos = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+      color = color + simbolos[Math.floor(Math.random() * 16)]
+    }
+    setcolor(color)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={onSubmit} />
+      <div
+        style={{
+          width: '200px',
+          height: '200px',
+          background: color,
+        }}
+        // onClick={onSubmit}
+        // onMouseEnter={onSubmit}
+        // onMouseOut={onSubmit}
+        // onChange={onSubmit}
+      ></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
